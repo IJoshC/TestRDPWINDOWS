a7fa17895449b93e01493410889187c8c85c4f3e
IJoshC <69088192+IJoshC@users.noreply.github.com>
2023-10-02 05:58:11 -0600

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,16 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
-& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
+
+# Descargar e instalar Chrome Remote Desktop Host
+$ChromeRemoteDesktopInstaller = $env:TEMP + '\chromeremotedesktophost.msi'
+Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $ChromeRemoteDesktopInstaller
+Start-Process $ChromeRemoteDesktopInstaller -Wait
+Remove-Item $ChromeRemoteDesktopInstaller
+
+# Descargar e instalar Chrome
+$ChromeInstaller = $env:TEMP + '\chrome_installer.exe'
+Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $ChromeInstaller
+Start-Process -FilePath $ChromeInstaller -Args '/install' -Verb RunAs -Wait
+Remove-Item $ChromeInstaller
+
+# Iniciar Chrome y acceder a la página web deseada
+Start-Process 'C:\Program Files\Google\Chrome\Application\chrome.exe' -ArgumentList 'https://twitch.tv/ijoshc'
